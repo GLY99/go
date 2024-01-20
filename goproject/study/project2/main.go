e9f556fc5828f2e6b2ec5d5f28ec32e932e15f9b
gouliyang <mr_gly@163.com>
2024-01-20 14:28:15 +0800

    go study

---
+++ goproject/study/project2/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("tom\tjack")
+	fmt.Println("tom\\jack")
+	fmt.Println("zhangsan\nlisi")
+	fmt.Println("who are you?\rlisi")
+	fmt.Println("name\tage\taddress\nzhangsan\t18\tbj")
+}
