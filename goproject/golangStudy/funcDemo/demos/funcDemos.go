9281c853b55f04e3eb55834a619f4632f885770b
gouliyang <mr_gly@163.com>
2024-02-26 22:47:19 +0800

    go study

--- goproject/golangStudy/funcDemo/demos/funcDemos.go
+++ goproject/golangStudy/funcDemo/demos/funcDemos.go
@@ -1,6 +1,8 @@
 package demos
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func Cal(num1 float64, num2 float64, operator byte) (res float64) {
 	// var res float64  // 如果在函数的返回类型列表中指定了变量相当于定义了一个变量，函数内不需要重复定义
@@ -17,4 +19,10 @@ func Cal(num1 float64, num2 float64, operator byte) (res float64) {
 		fmt.Printf("error operator...\n")
 	}
 	return res
+}
+
+func GetSumAndSub(num1 float64, num2 float64) (sum float64, sub float64) {
+	sum = num1 + num2
+	sub = num1 - num2
+	return sum, sub
 }
--- goproject/golangStudy/funcDemo/main/main.go
+++ goproject/golangStudy/funcDemo/main/main.go
@@ -8,4 +8,7 @@ import (
 func main() {
 	res := demo.Cal(1, 2, '+')
 	fmt.Printf("%.2f\n", res)
+	sum, _ := demo.GetSumAndSub(1.2, 0.2)
+	_, sub := demo.GetSumAndSub(1.2, 0.2)
+	fmt.Printf("sum=%.2f, sub=%.2f", sum, sub)
 }
