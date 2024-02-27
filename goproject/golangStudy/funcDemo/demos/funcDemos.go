8168032f1691f80b46a78d4bf3141fa7467b1954
gouliyang <mr_gly@163.com>
2024-02-27 22:45:56 +0800

    go study

--- goproject/golangStudy/funcDemo/demos/funcDemos.go
+++ goproject/golangStudy/funcDemo/demos/funcDemos.go
@@ -26,3 +26,27 @@ func GetSumAndSub(num1 float64, num2 float64) (sum float64, sub float64) {
 	sub = num1 - num2
 	return sum, sub
 }
+
+func FeboNums(num int) int {
+	if num == 1 || num == 2 {
+		return 1
+	}
+	return FeboNums(num-1) + FeboNums(num-2)
+}
+
+func GetFuncAsn(num int) int {
+	if num == 1 {
+		return 3
+	}
+	return 2*GetFuncAsn(num-1) + 1
+}
+
+func GetPeachs(day int) int {
+	if day > 10 || day < 1 {
+		return -1
+	}
+	if day == 10 {
+		return 1
+	}
+	return 2 * (GetPeachs(day+1) + 1)
+}
