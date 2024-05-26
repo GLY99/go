0a4788d8eb37541835d05843e0f336e59773602c
gouliyang <mr_gly@163.com>
2024-05-26 10:01:27 +0800

    go study

---
+++ goproject/leetcode/202405/imageSmoother/imageSmoother.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func imageSmoother(img [][]int) [][]int {
+	m, n := len(img), len(img[0])
+	ans := make([][]int, m)
+	for i := range ans {
+		ans[i] = make([]int, n)
+		for j := range ans[i] {
+			sum, num := 0, 0
+			for _, row := range img[max(i-1, 0):min(i+2, m)] {
+				for _, v := range row[max(j-1, 0):min(j+2, n)] {
+					sum += v
+					num++
+				}
+			}
+			ans[i][j] = sum / num
+		}
+	}
+	return ans
+}
+
+func main() {
+	fmt.Println(imageSmoother([][]int{{1, 1, 1}, {1, 0, 1}, {0, 0, 0}}))
+}
