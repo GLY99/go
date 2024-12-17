c188f764948e31b06e6ae3edea9764e0e6e34a43
gouliyang <mr_gly@163.com>
2024-12-17 22:00:25 +0800

    code study

---
+++ goproject/leetcode/202412/countEven.go
@@ -0,0 +1,15 @@
+package main
+
+func countEven(num int) int {
+	var ans int
+	for i := 1; i <= num; i++ {
+		sum := 0
+		for x := i; x > 0; x /= 10 {
+			sum += x % 10
+		}
+		if sum%2 == 0 {
+			ans++
+		}
+	}
+	return ans
+}
