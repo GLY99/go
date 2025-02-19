5eb8e2d6d5fa0179ba582928c3a8a281d9eaa6c7
gouliyang <mr_gly@163.com>
2025-02-19 23:41:17 +0800

    code study

---
+++ goproject/leetcode/202502/countLargestGroup.go
@@ -0,0 +1,25 @@
+package main
+
+// https://leetcode.cn/problems/count-largest-group/?envType=problem-list-v2&envId=hash-table
+
+func countLargestGroup(n int) int {
+	mapping := make(map[int]int)
+	maxCount := 0
+	for i := 1; i <= n; i++ {
+		sum := 0
+		val := i
+		for val > 0 {
+			sum += val % 10
+			val /= 10
+		}
+		mapping[sum]++
+		maxCount = max(maxCount, mapping[sum])
+	}
+	count := 0
+	for _, v := range mapping {
+		if v == maxCount {
+			count++
+		}
+	}
+	return count
+}
