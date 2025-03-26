709671788b7ae00a5f9bb4668436daf1c27fd2dc
gouliyang <mr_gly@163.com>
2025-03-26 22:08:15 +0800

    code study

---
+++ goproject/leetcode/202503/findEvenNumbers.go
@@ -0,0 +1,30 @@
+package main
+
+// https://leetcode.cn/problems/finding-3-digit-even-numbers/?envType=problem-list-v2&envId=sorting
+
+func findEvenNumbers(digits []int) []int {
+	mapping := make(map[int]int)
+	for _, digit := range digits {
+		mapping[digit]++
+	}
+	ans := []int{}
+	for i := 100; i <= 998; i += 2 {
+		tempMapping := map[int]int{}
+		temp := i
+		for temp > 0 {
+			tempMapping[temp%10]++
+			temp /= 10
+		}
+		flag := true
+		for k, v := range tempMapping {
+			if mapping[k] < v {
+				flag = false
+				break
+			}
+		}
+		if flag {
+			ans = append(ans, i)
+		}
+	}
+	return ans
+}
