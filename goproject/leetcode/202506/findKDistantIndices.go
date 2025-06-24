162d56b2943c51c308dde34eb0eed5cd4a16a0e7
gouliyang <mr_gly@163.com>
2025-06-24 22:06:04 +0800

    code study

---
+++ goproject/leetcode/202506/findKDistantIndices.go
@@ -0,0 +1,29 @@
+package main
+
+// https://leetcode.cn/problems/find-all-k-distant-indices-in-an-array/?envType=daily-question&envId=2025-06-24
+
+func findKDistantIndices(nums []int, key int, k int) []int {
+	ans := []int{}
+	for i := 0; i < len(nums); i++ {
+		s := 0
+		e := 0
+		if i-k < 0 {
+			s = 0
+		} else {
+			s = i - k
+		}
+		if i+k > len(nums)-1 {
+			e = len(nums) - 1
+		} else {
+			e = i + k
+		}
+		for s <= e {
+			if abs(i-s) <= k && nums[s] == key {
+				ans = append(ans, i)
+				break
+			}
+			s++
+		}
+	}
+	return ans
+}
