ae0e4e3eaca6f625402455fd4d813410d8b246be
gouliyang <mr_gly@163.com>
2025-09-15 22:49:57 +0800

    code study

---
+++ goproject/leetcode/202509/canBeTypedWords.go
@@ -0,0 +1,24 @@
+package main
+
+import "strings"
+
+// https://leetcode.cn/problems/maximum-number-of-words-you-can-type/?envType=daily-question&envId=2025-09-15
+
+func canBeTypedWords(text string, brokenLetters string) int {
+	brokens := new([26]int)
+	for _, c := range brokenLetters {
+		(*brokens)[c-'a']++
+	}
+	textArr := strings.Split(text, " ")
+	ans := 0
+lab1:
+	for _, word := range textArr {
+		for _, c := range word {
+			if (*brokens)[c-'a'] != 0 {
+				continue lab1
+			}
+		}
+		ans++
+	}
+	return ans
+}
